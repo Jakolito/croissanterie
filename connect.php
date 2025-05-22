2ca6e0b3490560adb210198f89a3fbaaafd73c42
Jakolito <jonathandamasco123456@gmail.com>
2025-05-22 23:41:51 +0800

    update

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 // local
 	// $conn = mysqli_connect("localhost", "root", "", "pastry1");
 // production
-	$conn = mysqli_connect("localhost", "Croiss@2025", "Croiss_2025", "u801377270_croiss_2025");
+	$conn = mysqli_connect("localhost", "u801377270_croiss_2025", "Croiss_2025", "u801377270_croiss_2025");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }

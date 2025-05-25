5376881d7c164267a38517f20ec670990f2bdbb0
Jakolito <jonathandamasco123456@gmail.com>
2025-05-25 22:04:41 +0800

    try ulit

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 // local
-	$conn = mysqli_connect("localhost", "root", "", "pastry1");
+	//$conn = mysqli_connect("localhost", "root", "", "pastry1");
 // production
-	// $conn = mysqli_connect("localhost", "u801377270_croiss_2025", "Croiss_2025", "u801377270_croiss_2025");
+	 $conn = mysqli_connect("localhost", "u801377270_croiss_2025", "Croiss_2025", "u801377270_croiss_2025");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
